18784a76fdda8b167c4b8336fd675b58ee08bf3b
Ninja-Joker <134727338+Ninja-Joker@users.noreply.github.com>
2023-05-26 20:51:52 +0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,30 @@
+function click1 ()
+{
+    console.log("This Is Onclick")
+    alert("This Is Onclick")
+}
+function load1load ()
+{
+    console.log("This Is Onload")
+    alert("This Is Onload")
+}
+function submit1 ()
+{
+    console.log("This Is Onsubmit")
+    alert("This Is Onsubmit")
+}
+function mouseover ()
+{
+    console.log("This Is Onmouseover")
+    alert("This Is Onmouseover")
+}
+function mousedown ()
+{
+    console.log("This Is Onmousedown")
+    alert("This Is Onmousedown")
+}
+function mouseup ()
+{
+    console.log("This Is Onmpuseup")
+    alert("This Is Onmouseup")
+}
